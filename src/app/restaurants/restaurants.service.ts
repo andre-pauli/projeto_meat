ab90e52e81def195d940d980c3d73a52e920d0e1
André Pauli <andreps25@gmail.com>
2019-06-18 09:07:59 -0300

    Segundo Commit - Alterações e validações no formulário

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -9,6 +9,7 @@ import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
 
 import { ErrorHandler } from '../app.error-handler'
+import { MenuItem } from 'app/restaurant-detail/menu-item/menu-item.model';
 
 @Injectable()
 export class RestaurantsService {
@@ -19,5 +20,23 @@ export class RestaurantsService {
         return this.http.get(`${MEAT_API}/restaurants`)
             .map(response => response.json())
             .catch(ErrorHandler.handleError)
+    }
+
+    restaurantById(id: string): Observable<Restaurant> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
+    }
+
+    reviewsOfRestaurant(id: string): Observable<any> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+            .map(response => response.json())
+            ._catch(ErrorHandler.handleError)
+    }
+    
+    menusOfRestaurant(id: string): Observable<MenuItem[]> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+            .map(response => response.json())
+            ._catch(ErrorHandler.handleError)
     }
 }
