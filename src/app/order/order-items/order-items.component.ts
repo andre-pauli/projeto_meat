5cffb745dc99c17f56882fb2b623c6c8cccc84d8
André Pauli <andreps25@gmail.com>
2019-06-18 14:30:58 -0300

    Criando componente order-items que deixará dinâmico os itens da finalização da compra

---
+++ src/app/order/order-items/order-items.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
+
+@Component({
+  selector: 'mt-order-items',
+  templateUrl: './order-items.component.html'
+})
+export class OrderItemsComponent implements OnInit {
+
+  items: CartItem[]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
