4765ea7757777a28c8871033ac787d25c1261b0a
André Pauli <andreps25@gmail.com>
2019-06-27 14:28:15 -0300

    Atualizações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,39 +2,27 @@ import { LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { AboutComponent } from './about/about.component';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ROUTES } from './app.routes';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { DeliveryCostComponent } from './order/delivery-cost/delivery-cost.component';
-import { OrderItemsComponent } from './order/order-items/order-items.component';
-import { OrderComponent } from './order/order.component';
-import { OrderService } from './order/order.service';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
-import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
-import { RestaurantsService } from './restaurants/restaurants.service';
-import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
-import { RatingComponent } from './shared/rating/rating.component';
-
-
-
+import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent,
     RestaurantsComponent,
     RestaurantComponent,
     RestaurantDetailComponent,
@@ -42,22 +30,16 @@ import { RatingComponent } from './shared/rating/rating.component';
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent,
-    InputComponent,
-    RadioComponent,
-    OrderItemsComponent,
-    DeliveryCostComponent,
-    OrderSummaryComponent,
-    RatingComponent
+    OrderSummaryComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpModule,
-    ReactiveFormsModule,
-    FormsModule,
-    RouterModule.forRoot(ROUTES)
+    SharedModule.forRoot(),
+    RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules })
   ],
-  providers: [RestaurantsService, ShoppingCartService, OrderService, ReactiveFormsModule, { provide: LOCALE_ID, useValue: 'pt-BR' }],
+  providers: [ReactiveFormsModule, { provide: LOCALE_ID, useValue: 'pt-BR' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/order/order.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { OrderComponent } from './order.component';
+import { OrderItemsComponent } from './order-items/order-items.component';
+import { DeliveryCostComponent } from './delivery-cost/delivery-cost.component';
+import { SharedModule } from 'app/shared/shared.module';
+import { RouterModule, Routes } from '@angular/router';
+
+const ROUTES: Routes = [
+    { path: '', component: OrderComponent }
+]
+
+@NgModule({
+    declarations: [OrderComponent, OrderItemsComponent, DeliveryCostComponent],
+    imports: [SharedModule, RouterModule.forChild(ROUTES)],
+    exports: [],
+    providers: [],
+})
+
+export class OrderModule { }
